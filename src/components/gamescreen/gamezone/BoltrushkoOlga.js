76fc23303aa9b6d7998448b895719fa987cfc8cb
savchukeugene <savchukeugene.work@gmail.com>
2024-04-18 02:07:44 +0300

    update drag and drop function

--- src/components/gamescreen/gamezone/BoltrushkoOlga.js
+++ src/components/gamescreen/gamezone/BoltrushkoOlga.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo0 from "./img/0.png"
 import logo1 from "./img/1.png"
 import logo2 from "./img/2.png"
@@ -7,7 +7,6 @@ function BoltrushkoOlga() {
   const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
   const columns = Array.from({ length: 10 }, (_, index) => index + 1);
   const logoArr = [logo0, logo1, logo2]
-  const [showLogo, setShowLogo] = useState(true)
   const generateRandomPicture = () => {
     let res = Math.floor(Math.random() * 3);
     console.log(res);
@@ -15,28 +14,25 @@ function BoltrushkoOlga() {
   };
 
   return (
-    <div className="gamezone" id="enemyPlayer">
+    <div className="gamezone" id="currentPlayer">
       <div className="cell"></div>
       {columns.map((col) => (
-        <div key={col} className="cell cord">
+        <div key={col} className="cell">
           {col}
         </div>
       ))}
       {rows.map((row) => (
         <React.Fragment key={row}>
-          <div className="cell cord">{row}</div>
+          <div className="cell">{row}</div>
           {columns.map((col) => (
             <div
               key={`${row}${col}`}
               style={{
                 background: `url(${logoArr[Math.floor(Math.random() * 3)]})`,
               }}
-              onClick={(e) => {
-                e.target.style.background = "none"
-              }}
-              className="cell"
-              id={`${row}${col}`
-              }
+              
+              className="cell hov"
+              id={`${row}${col}`}
             ></div>
           ))}
         </React.Fragment>
--- src/components/gamescreen/gamezone/YatchukAndrey.js
+++ src/components/gamescreen/gamezone/YatchukAndrey.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function YatchukAndrey() {
   const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
-  const columns = Array.from({ length: 10 }, (_, index) => index + 1);
+  const columns = Array.from({ length: 10 }, (_, index) => index);
   let box = document.getElementById('box')
 
   return (
@@ -10,22 +10,30 @@ function YatchukAndrey() {
       <div className='displayNone' id="box"></div>
       <div className="cell"></div>
       {columns.map((col) => (
-        <div draggable="false" key={col} className="cell cord">
+        <div draggable="false" key={col} className="cell cord" id={`N${col}`}>
           {col}
         </div>
       ))}
       {rows.map((row, rowIndex) => (
         <React.Fragment key={row}>
-          <div draggable="false" className="cell cord">{row}</div>
+          <div draggable="false" className="cell cord" id={`L${rowIndex + 1}`}>{row}</div>
           {columns.map((col, colIndex) => (
             <div 
               key={`${rowIndex}${colIndex}`} 
               onDragOver={(e) => {
-                e.target.style.boxShadow = "0px 0px 5px green inset"
+                  e.target.style.boxShadow = "0px 0px 5px rgb(175, 175, 255) inset"
+                  let numberCell = document.getElementById(`N${e.target.id[1]}`)
+                  numberCell.style.backgroundColor = 'white'
+                  let letterCell = document.getElementById(`L${e.target.id[0]}`)
+                  letterCell.style.backgroundColor = 'white'
               }}
               onDragLeave={(e) => {
                 // box.classList.add("displayNone")
-                e.target.style.boxShadow = "0px 0px 5px rgb(23, 23, 23) inset"
+                  e.target.style.boxShadow = "0px 0px 5px rgb(23, 23, 23) inset"
+                  let numberCell = document.getElementById(`N${e.target.id[1]}`)
+                  numberCell.style.backgroundColor = 'rgb(175, 175, 255)'
+                  let letterCell = document.getElementById(`L${e.target.id[0]}`)
+                  letterCell.style.backgroundColor = 'rgb(175, 175, 255)'
               }}
               className="hov cell" 
               id={`${rowIndex + 1}${col}`}
