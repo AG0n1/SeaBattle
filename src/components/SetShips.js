bd9980ec8f2eb73604284392a35e43a71eabf07d
mathew markovets <mmarkovec15072003@gmail.com>
2024-04-29 01:00:09 +0300

    Fixed SetShips component

--- src/components/SetShips.js
+++ src/components/SetShips.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 import YatchukAndrey from './gamescreen/gamezone/YatchukAndrey';
 import BazevichDavid from './gamescreen/gamezone/BazevichDavid';
 function App() {
+
+    const [amount, setAmount] = useState(0)
+    const checkState = (e) => {
+        if (amount != 0) {
+            alert(
+                "Разместите верное количество кораблей"
+            )
+            e.preventDefault()
+        }
+      }
     return (
-            <div className="setShips">
+        <div className="setShips">
+            <div></div>
+
+            <div className="makeSpace">
                 <YatchukAndrey />
-                <BazevichDavid />
-            </div>
+
+                <div className='link' id = "makeLinkDNone">
+                    <Link onClick={checkState} className='linkToPlay' to="../../play">Играть</Link>
+                </div>
+            </div>      
+
+            <BazevichDavid />
+        </div>
     );
 }
-
-export default App;
