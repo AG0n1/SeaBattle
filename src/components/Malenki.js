c298314ebc7cf1052d3f487b3d7477c06d4df44f
mathew markovets <mmarkovec15072003@gmail.com>
2024-04-29 11:45:12 +0300

    Some changes

--- src/components/Malenki.js
+++ src/components/Malenki.js
@@ -12,9 +12,12 @@ function Malenki(props) {
     return (
         <div className="video" onclick="this.querySelector('video').play()">
             <video src={video} id="vidos" autoPlay></video>
-            <button className="playButton" onClick={playVideo}><img src="./components/gamescreen/gamezone/img/Button.jpg" />
+            <button className="playButton" onClick={playVideo}>
+                <img src="./components/gamescreen/gamezone/img/Button.jpg" />
             </button>
-            <Link to="../play" className="skipButton">Skip</Link>
+            <Link to="../setShips" className="skipButton">
+                Skip
+            </Link>
         </div>
     )
 }
