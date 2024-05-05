ef247c9df1f0f809628a00dca72e1752fc2e579c
mathew markovets <mmarkovec15072003@gmail.com>
2024-05-05 17:47:46 +0300

    Created Point class

--- server.js
+++ server.js
@@ -2,15 +2,59 @@ const express = require('express');
 const cors = require('cors')
 const http = require('http');
 const path = require('path');
+const events = require('events')
 const localtunnel = require('localtunnel');
+
+const emitter = events.EventEmitter()
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 const server = http.createServer(app);
 
-let games = {
+class Point {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y
+    }
+}
 
+let point1 = new Point(1,2)
+let point2 = new Point(1,3)
+let points = [point1, point2]
+console.log(points)
+
+// TEST CASES!!!!!!!!
+const positions = {
+    0: {type: 'double', x: 2, y: 8},
+    1: {type: 'single', x: '1', y: '8'},
+    2: {type: 'single', x: '0', y: '8'},
+    3: {type: 'single', x: '1', y: '9'},
+    4: {type: 'single', x: '0', y: '9'},
+    5: {type: 'double', x: 5, y: 5},
+    6: {type: 'double', x: 3, y: 5},
+    7: {type: 'triple', x: 5, y: 1},
+    8: {type: 'triple', x: 5, y: 1},
+    9: {type: 'ultimate', x: 5, y: 1},
+}
+
+const ableToHit = () => {
+    positions.forEach(element => {
+        switch(element.type) {
+            case "double":
+                break;
+            
+            case "tripple":
+                break;
+            
+            case "ultimate":
+                break;
+        }
+    });
+}
+
+let games = {
+    
 }
 
 app.post('/createGame', (req, res) => {
@@ -18,8 +62,13 @@ app.post('/createGame', (req, res) => {
     let {gameId} = req.body,
         {name} = req.body
     games[gameId] = {
-        name1: name
-    }  
+        [name]: {
+            isReady: false,
+            pos: {
+                
+            }
+        }
+    }
 })
 
 app.post('/connectToGame', (req, res) => {
@@ -31,14 +80,31 @@ app.post('/connectToGame', (req, res) => {
         console.log('Game found:', game);
         res.json({isFind: true})
 
-        games[gameId].name2 = name
+        games[gameId][name] = {
+            isReady: false,
+            pos: {
+                
+            }
+        }
     } else {
         res.json({isFind: false})
     }
 });
 
+app.get('/shoot', (req,res) => {
+    emitter.once('newMessage', (message) => {
+        res.json(message)
+    })
+})
+
+app.post('/step', (req, res) => {
+    const msg = req.body
+    emitter.emit('newMessage', )
+    res.status(200)
+})
+
 app.get('/api', (req, res) => {
-    res.json(games)
+    res.json(points)
 })
 
 app.post('/api', (req, res) => {
