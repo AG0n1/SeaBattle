3afc3e2d66072a57deb703f5344a029b9c1b8aa4
mathew markovets <mmarkovec15072003@gmail.com>
2024-05-05 21:23:19 +0300

    Complete points

--- server.js
+++ server.js
@@ -13,18 +13,42 @@ app.use(express.json())
 const server = http.createServer(app);
 
 class Point {
-    constructor(x,y) {
+    constructor(x, y) {
         this.x = x;
-        this.y = y
+        this.y = y;
+        this.pointsArr = [];
+    }
+
+    createPointsArr(positions) {
+        for (const key in positions) {
+            if (Object.hasOwnProperty.call(positions, key)) {
+                const { type, x, y } = positions[key];
+                if (type === 'double') {
+                    this.pointsArr.push({ x: Number(x), y: Number(y) });
+                    this.pointsArr.push({ x: Number(x), y: Number(y) + 1 });
+                } else if (type === 'triple') {
+                    this.pointsArr.push({ x: Number(x), y: Number(y) });
+                    this.pointsArr.push({ x: Number(x), y: Number(y) + 1 });
+                    this.pointsArr.push({ x: Number(x), y: Number(y) + 2 });
+                } else if (type === 'ultimate') {
+                    this.pointsArr.push({ x: "Here is ultimate" });
+                    for (let i = 0; i < 3; i++) {
+                        for (let j = 0; j < 4; j++) {
+                            this.pointsArr.push({ x: Number(x) + i, y: Number(y) + j });
+                        }
+                    }
+                    this.pointsArr.push({ x: "Here is ultimate" });
+                } else {
+                    this.pointsArr.push({ x: Number(x), y: Number(y) });
+                }
+            }
+        }
+        return this.pointsArr;
     }
 }
 
-let point1 = new Point(1,2)
-let point2 = new Point(1,3)
-let points = [point1, point2]
-console.log(points)
+const point = new Point
 
-// TEST CASES!!!!!!!!
 const positions = {
     0: {type: 'double', x: 2, y: 8},
     1: {type: 'single', x: '1', y: '8'},
@@ -38,6 +62,8 @@ const positions = {
     9: {type: 'ultimate', x: 5, y: 1},
 }
 
+let points = point.createPointsArr(positions)
+console.log(points)
 const ableToHit = () => {
     positions.forEach(element => {
         switch(element.type) {
